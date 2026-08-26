832741dbe5a92e0a599627a364bb41d6a49d8e52
terenceyappchengfei <terence243051@dasein.edu.my>
2026-08-27 01:19:44 +0800

    feat: added opening and closing for Anvil UI for interaction testing

--- Assets/Script/StationInteraction.cs
+++ Assets/Script/StationInteraction.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class StationInteraction : MonoBehaviour
 {
+
+    public GameObject stationPanel;
+
+    public void OpenStation()
+    {
+        stationPanel.SetActive(true);
+    }
+
+    public void CloseStation()
+    {
+        stationPanel.SetActive(false);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
