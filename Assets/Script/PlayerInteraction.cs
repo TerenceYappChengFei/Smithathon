6fd592a39ce681c37831662891645cb4ec2ec371
terenceyappchengfei <terence243051@dasein.edu.my>
2026-08-27 14:42:56 +0800

    feat: refined interact button detection, and made interact button becomes transparent when no detection is made

--- Assets/Script/PlayerInteraction.cs
+++ Assets/Script/PlayerInteraction.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private StationInteraction currentStation;
+    public CanvasGroup interactButton; //for interact button behaviours
+
+
+    private void Start()
     {
-        
+        SetInteractButton(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        StationInteraction station =
+            other.GetComponent<StationInteraction>();
+
+        if (station != null)
+        {
+            currentStation = station;
+            SetInteractButton(true);
+            //Debug.Log("Station is available");
+        }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StationInteraction station =
+            other.GetComponent<StationInteraction>();
+
+        if (station == currentStation)
+        {
+            currentStation = null;
+            SetInteractButton(false);
+            //Debug.Log("Station is no longer available");
+        }
+    }
+
+    public void Interact()
+    {
+        if (currentStation != null)
+        {
+            currentStation.OpenStation();
+        }
+    }
+
+    private void SetInteractButton(bool available)
+    {
+        if (available)
+        {
+            interactButton.alpha = 1f;
+            interactButton.interactable = true;
+            interactButton.blocksRaycasts = true;
+        }
+        else
+        {
+            interactButton.alpha = 0.3f;
+            interactButton.interactable = false;
+            interactButton.blocksRaycasts = false;
+        }
+    }
+
 }
--- Assets/Script/StationInteraction.cs
+++ Assets/Script/StationInteraction.cs
@@ -14,16 +14,4 @@ public class StationInteraction : MonoBehaviour
     {
         stationPanel.SetActive(false);
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
